877b465330aa4975345ced8d09f88b8ac0cf71ce
Tejas Sonawane <105315165+sonawaneteju4@users.noreply.github.com>
2023-08-31 17:04:31 +0530

    .

---
+++ .history/web-app/src/components/CreateUser_20230831161757.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { useState } from "react";
+import {createUserWithEmailAndPassword} from 'firebase/auth'
+import { auth } from "../firebaseConfig";
+
+const CreateUser = () => {
+  const [reg, setReg] = useState({email :"" ,password: "123"})
+  // const [user, setuser] = useState({});
+  
+
+  const register = async() => {
+    try {
+      const user = await createUserWithEmailAndPassword(auth , reg.email , reg.password);
+      console.log(user)
+    } catch (error) {
+      console.log(error.message)
+    }
+
+  }
+
+  const handleChange = (e) =>{
+    setReg({...reg , [e.target.name]: e.target.value})
+  }
+
+
+  return (
+    <div>
+      <div>
+        <label>Email</label>
+        <input type="email" name="email" onChange={handleChange}></input>
+      </div>
+      <div>
+        <label>Password</label>
+        <input type="password" name='password' onChange={handleChange}></input>
+      </div>
+      <div>
+        <button onClick={register} >CreateUser</button>
+      </div>
+      {/* <p>{reg.email}</p>
+      <p>{reg.password}</p> */}
+    </div>
+  );
+};
+
+export default CreateUser;
