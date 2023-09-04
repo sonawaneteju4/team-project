088c522f00f4855247ed14dd9ce985283d1a47e7
Tejas Sonawane <105315165+sonawaneteju4@users.noreply.github.com>
2023-09-04 11:36:02 +0530

    .

--- .history/web-app/src/components/hospital/HospitalRegister_20230904113540.js
+++ .history/web-app/src/components/hospital/HospitalRegister_20230904113540.js
@@ -29,24 +29,7 @@ const HospitalRegister = () => {
 
   //for state and dist
 
-  const handleStateChange = (event) => {
-    const newState = event.target.value;
-    setSelectedState(newState);
-    setregUser({
-      ...regUser,
-      state: newState,
-    });
-    setSelectedDistrict(""); // Reset the district when the state changes
-  };
-
-  const handleDistrictChange = (event) => {
-    const newDistrict = event.target.value;
-    setSelectedDistrict(newDistrict);
-    setregUser({
-      ...regUser,
-      dist: newDistrict,
-    });
-  };
+  
 
 
   const handleChange = (e) => {
