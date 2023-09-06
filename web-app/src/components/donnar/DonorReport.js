a705cc7cc7c933720d6dd5bfaf5055dd953493c5
ashvinisisodiya27 <107295976+ashvinisisodiya27@users.noreply.github.com>
2023-09-06 16:13:34 +0530

    .

--- web-app/src/components/donnar/DonorReport.js
+++ web-app/src/components/donnar/DonorReport.js
@@ -4,11 +4,12 @@ import { db } from '../../firebaseConfig'
 
 const DonorReport = () => {
     const [ReqData,setReqData]=useState([])
-    const DataRef=collection(db,'   ')
+    const DataRef=collection(db,'donorReport')
     useEffect(() => {
       const q=query(DataRef,where('uId','==',localStorage.getItem('userId')))
       const  getReport=async()=>{
         const data=await getDocs(q)
+        console.log(data)
         console.log((items)=>{
             setReqData(items.data())
         })
@@ -21,7 +22,7 @@ const DonorReport = () => {
     <div>
         {ReqData.map((items)=>(
             <div>
-                
+
             </div>
         )
         )}
