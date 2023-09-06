49a72db95927a2ed1448ba7109a647adb4a38a36
ashvinisisodiya27 <107295976+ashvinisisodiya27@users.noreply.github.com>
2023-09-06 16:10:00 +0530

    .

---
+++ web-app/src/components/donnar/DonorReport.js
@@ -0,0 +1,32 @@
+import { collection, getDocs, query, where } from 'firebase/firestore'
+import React, { useEffect, useState } from 'react'
+import { db } from '../../firebaseConfig'
+
+const DonorReport = () => {
+    const [ReqData,setReqData]=useState([])
+    const DataRef=collection(db,'   ')
+    useEffect(() => {
+      const q=query(DataRef,where('uId','==',localStorage.getItem('userId')))
+      const  getReport=async()=>{
+        const data=await getDocs(q)
+        console.log((items)=>{
+            setReqData(items.data())
+        })
+      }
+    getReport();
+        
+      },[])
+
+  return (
+    <div>
+        {ReqData.map((items)=>(
+            <div>
+                
+            </div>
+        )
+        )}
+    </div>
+  )
+}
+
+export default DonorReport
