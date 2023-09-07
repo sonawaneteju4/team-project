01c69553c422339a537fa8ea81300839c2cf99c8
ashvinisisodiya27 <107295976+ashvinisisodiya27@users.noreply.github.com>
2023-09-07 17:10:25 +0530

    .

--- web-app/src/components/Index.js
+++ web-app/src/components/Index.js
@@ -9,13 +9,13 @@ const Index = () => {
 
      
    
-    { image: "./image/image2.jpg", caption: "Bike Wraping" },
-    { image: ".//image/image3.jpg", caption: "Mobile Wraping" },
-    { image: "./image/image4.jpg", caption: "Alloy Painting" },
-    {image:"./image/image5.jpg",caption:"abcd"},
-    {image:"./image/image6.jpg",caption:"abcd"},
-    {image:"./image/image7.jpg",caption:"abcde"},
-    {image:"./image/image8.jpg",caption:"xyz"}
+    { image: "./image/image2.jpg", caption: "Together, we can make a world full of life." },
+    { image: ".//image/image3.jpg", caption: "Safety first, for you and for them – donate blood responsibly" },
+    { image: "./image/image4.jpg", caption: "A drop of kindness can create an ocean of difference" },
+    {image:"./image/image5.jpg",caption:"Your blood is their lifeline; donate generously"},
+    {image:"./image/image6.jpg",caption:"Your blood has the power to heal"},
+    {image:"./image/image7.jpg",caption:"A safe donation is a compassionate donation"},
+    {image:"./image/image8.jpg",caption:"Don't just give blood; give safe blood"}
   ];
   return (
     <div>
